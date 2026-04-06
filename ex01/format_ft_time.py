7ee7ac439f7377baba5cfed5c53b688b40df8f72
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-06 22:49:36 +0200

    Add: starting to print whats printable yet

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,3 +1,9 @@
 import time
 
-print(time.localtime())
+# https://stackoverflow.com/questions/4548684/how-to-get-the-seconds-since-epoch-from-the-time-date-output-of-gmtime
+
+print("Seconds since January 1, 1970: ", end = "")
+print(time.time(), end = "")
+print(" or", end = "")
+print("  ...   ", end = "")
+print(" in scientific notation", end = "")
