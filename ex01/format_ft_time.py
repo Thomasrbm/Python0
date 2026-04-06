1141cd60f80881c18d79dde70c72d57ddad62452
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-06 23:04:38 +0200

    add: Advancing parsing

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,9 +1,16 @@
 import time
+import datetime
 
 # https://stackoverflow.com/questions/4548684/how-to-get-the-seconds-since-epoch-from-the-time-date-output-of-gmtime
 
+# https://www.programiz.com/python-programming/datetime/strftime
+
+
 print("Seconds since January 1, 1970: ", end = "")
-print(time.time(), end = "")
+sec = time.time()
+print(sec, end = "")
 print(" or", end = "")
 print("  ...   ", end = "")
-print(" in scientific notation", end = "")
+print(" in scientific notation")
+x = datetime.datetime.now()
+print(x.strftime("%b %d %Y")) 
