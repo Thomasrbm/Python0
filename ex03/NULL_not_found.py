e1e2580543c6453d8422a32bfc6ead5135f3a717
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-08 13:25:17 +0200

    Add: ex03, NULL types

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,23 +1,21 @@
 
 
 
-
-
 def NULL_not_found(object: any) -> int:
-    Nothing = None
-    print(Nothing == None)
-    return 0
+    x = type(object)
+    if object is None:
+        print(f"Nothing : {object} {x}")
+    elif type(object) is float and object != object:
+        print(f"Cheese: {object}  {x}")
+    elif type(object) is int and object == 0:
+        print(f"Zero: {object} {x}")
+    elif type(object) is str and object == "":
+        print(f"Empty: {object} {x}")
+    elif type(object) is bool and object is False:
+        print(f"Fake: {object} {x}")
+    else:
+        print("Type not Found")
+        return 1
 
-if __name__ == "__main__":
-    NULL_not_found(0)
-
-# try
-# {
-
-
-# }
-# catch()
-# {
-
-# }
+    return 1
 
