4233e5f42b27d39640baad260a2a5eaf91bae954
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-15 01:29:47 +0200

    add: training special attribute done

--- exos/spe_attrib.py
+++ exos/spe_attrib.py
@@ -24,12 +24,34 @@
 
 # Niveau 2 : Inspecter les entrailles (__dict__ et __class__)
 
+# chaque variable d une classe est stock dans un dico appele dict
+
+# class Smartphone:
+#     def __init__(self, marque, modele):
+#         self.marque = marque
+#         self.modele = modele
+
+# tel = Smartphone("Apple", "iPhone 15")
+
+# print(tel.__dict__)
+
+# print(tel.__class__.__name__)
 
 
 
 
 
 # Niveau 3 : L'objet qui se prend pour une fonction (__call__)
+
+# class Multiplicateur():
+# 	def __init__(self, n) :  # attribu quand on cree l instance le n
+# 		self.n = n
+# 	def __call__(self, mult): # attribe le mult quand on l use comme fonction
+# 		return self.n * mult
+
+# double = Multiplicateur(2)
+# print(double(10)) # Doit afficher 20
+
 
 
 
@@ -38,16 +60,116 @@
 
 
 
+
+# class Equipe():
+# 	def __init__(self, membres):
+# 		self.membres = membres
+# 	def __len__(self):
+# 		return len(self.membres)
+# 	def __getitem__(self, n):
+# 		return self.membres[n]
+
+
+# team = Equipe(['leo', 'roro', 'neyney'])
+
+# print(len(team))
+
+# print(team[0])
+
+
+
+
+
+
+
+
+
+
 # Niveau 5 : La doc et les fichiers (__doc__ et __file__)
 
 
-# Niveau 6 : Le plus utile : __name__
+# def nul():
+# 	"""rien"""
+# 	pass
+
+
+# print(nul.__doc__)
+# print(__file__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 # Niveau 7 : Le plan de bataille : __mro__ (Method Resolution Order)
 
 
+# class A:
+#     def saluer(self):
+#         print("Hello de A")
+
+# class B(A):
+#     def saluer(self):
+#         print("Hello de B")
+
+# class C(A):
+#     def saluer(self):
+#         print("Hello de C")
+
+# class D(B, C):
+#     def saluer(self):
+#         super().saluer() # permet d appeler celui du parent aussi
+#         print("Hello de D")
+
+# objet_d = D()
+# objet_d.saluer()
+
+# print("\nPlan de bataille (MRO) :")
+# print(D.__mro__)
+
 # Niveau 8 : L'optimiseur de mémoire : __slots__
 
-# Niveau 9 : Le contrôleur d'import : __all__
+
+
+
+
+# class JoueurNormal():
+# 	def __init__(self, pseudo, score):
+# 		self.pseudo = pseudo
+# 		self.score = score
+
+
+
+# class JoueurOptimise():
+# 	__slots__ = ('pseudo', 'score')
+# 	def __init__(self, pseudo, score):
+# 		self.pseudo = pseudo
+# 		self.score = score
+	
+
+
+# j1 = JoueurNormal("tueur", 5000)
+
+# print(j1.__dict__)
+
+# j1.couleur = "rouge"
+
+# print(j1.__dict__)
+
+
+
+# j2 = JoueurOptimise("Roro", 100)
+# j2.age = 25
+
