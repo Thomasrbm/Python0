55a12e3e8c4d8b279388d20872ccb40e3c7a6caa
thomasrbm <frozyxyt.76@gmail.com>
2026-04-15 11:20:51 +0200

    add: training sum

---
+++ exos/sum.py
@@ -0,0 +1,39 @@
+
+# ex 1 --------------------------------
+
+
+# prix = [19.99, 5.50, 10.0, 2.75]
+
+
+# total = sum(price for price in prix)
+# print(total)
+
+
+
+# ex 2 --------------------------------
+
+
+
+# nombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# pair_total = sum(pair for pair in nombres if pair % 2 == 0)
+
+# print(pair_total)
+
+
+# ex 3 --------------------------------
+
+
+# matrice = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+
+# total = sum(elem for lst in matrice for elem in lst)
+# print(total)
+
+
+
